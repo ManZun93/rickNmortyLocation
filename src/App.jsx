aed6ed8ad79ff61dac16133fc3494505e75a7501
ManZun93 <manuellufan19@gmail.com>
2022-11-04 23:23:40 -0500

    subiendo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { useEffect } from 'react'
+import axios from 'axios'
+import ResidentInfo from './components/ResidentInfo'
+
+function App() {
+  const [rickNmorty, setRickNmorty] = useState({})
+  const [typeId, setTypeId] = useState("")
+
+  useEffect (()=> {
+    const randomId = Math.floor(Math.random( )* 126) + 1
+    axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
+    .then (res => setRickNmorty(res.data));
+
+  }, [])
+
+ 
+
+  const searchId = () => {
+    axios.get(`https://rickandmortyapi.com/api/location/${typeId}`)
+    .then (res => setRickNmorty(res.data));
+
+    if(typeId > 126) {
+      alert("No Existe ese id")
+    }
+  }
+
+
+    
+ 
+
+  return (
+    <div className="App">
+       
+      <header>
+          <h1> LOCATIONS</h1>
+      </header>
+     
+
+      <div className='input-search'>
+        <input type="text"  
+          value={typeId}
+          placeholder = "Type a location id (1 to 126)" 
+          onChange = {e => setTypeId(e.target.value) }/>
+        <button onClick={searchId} onKeyDown= {searchId}>Search</button>
+        
+      </div>
+
+      
+     
+
+      <div className='location-info'>
+        <h2>
+          <b>{rickNmorty.name}</b>
+        </h2>
+
+        <h3>
+          Type : {rickNmorty.type}
+        </h3>
+
+        <h3>
+          Dimension : {rickNmorty.dimension} 
+        </h3>
+
+        <h3>
+          Residents Quantity : {rickNmorty.residents?.length}
+        </h3>
+
+        <div className='underline'></div>
+
+      </div>
+
+      
+      <ul>
+        {rickNmorty.residents?.map(residentUrl => (
+          <ResidentInfo 
+            url = {residentUrl}
+            key = {residentUrl}
+          />)
+        )}
+      </ul>
+      
+
+
+      <footer>
+        By Manuel Lufan
+      </footer>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ResidentInfo.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const ResidentInfo = ({url}) => {
+
+const [character, setCharacter] = useState({});
+
+useEffect (()=> {
+  axios.get(url)
+  .then (res => setCharacter(res.data))
+},[])
+
+console.log(character)
+
+
+
+
+
+
+
+  return (
+    <ul>
+      <li >
+
+        <h3>{character.name} </h3> <br />
+        <img className='card-image' src={character?.image} alt="" /> 
+        <p> Status: {character.status} </p>
+        <p> Origin: {character.origin?.name} </p>
+        <p> Episodes appareances: {character.episode?.length}</p>
+        
+
+        
+
+
+        
+      </li>
+    </ul>
+  );
+};
+
+export default ResidentInfo;
